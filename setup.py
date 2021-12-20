568f2d41ce0062851a60e4b56e584a7c3602c61f
pnadolny13 <patnadolny@gmail.com>
2021-12-20 15:47:42 -0500

    add all file contents

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="files-superset",
+    version="0.1",
+    description="Meltano project files for Superset",
+    packages=find_packages(),
+    package_data={
+        "bundle": [
+            "analyze/superset/README.md",
+            "analyze/superset/*",
+            "analyze/superset/docker/*",
+            "analyze/superset/docker/.env",
+            "analyze/superset/docker/.env-non-dev",
+
+        ]
+    },
+)
